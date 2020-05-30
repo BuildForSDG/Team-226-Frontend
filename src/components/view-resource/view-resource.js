1983e0d9e71df549bdda4634c9dc57fc48775540
Cressence <cressencey@gmail.com>
2020-05-31 00:59:32 +0100

    feat: remove logged header as sticky

--- src/components/view-resource/view-resource.js
+++ src/components/view-resource/view-resource.js
@@ -22,6 +22,20 @@ const ViewResource = () => {
           roll cupcake tart pastry. Chocolate marzipanpudding lollipop. Muffin
           ice cream gummies chupa chups halvah toffee candy canes cand.
         </p>
+        <p className="">
+          Fruitcake tart carrot cake jelly beans chocolate Sweet roll cupcake tart pastry.
+          Chocolate marzipanpuddinglollipop. Muffin ice cream gummies chupa chups halvah
+          toffee candy canes cand. Fruitcake tart carrot cake jelly beans chocolate Sweet
+          roll cupcake tart pastry. Chocolate marzipanpudding lollipop. Muffin
+          ice cream gummies chupa chups halvah toffee candy canes cand.
+        </p>
+        <p className="">
+          Fruitcake tart carrot cake jelly beans chocolate Sweet roll cupcake tart pastry.
+          Chocolate marzipanpuddinglollipop. Muffin ice cream gummies chupa chups halvah
+          toffee candy canes cand. Fruitcake tart carrot cake jelly beans chocolate Sweet
+          roll cupcake tart pastry. Chocolate marzipanpudding lollipop. Muffin
+          ice cream gummies chupa chups halvah toffee candy canes cand.
+        </p>
         <iframe
           width="100%"
           height="278"
