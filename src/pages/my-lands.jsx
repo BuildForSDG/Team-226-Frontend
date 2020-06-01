32be946cb55d0054a498579af72f98d48d0ee9dc
Cressence <cressencey@gmail.com>
2020-06-01 23:53:36 +0100

    feat: complete delete land

--- src/pages/my-lands.jsx
+++ src/pages/my-lands.jsx
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import notificationService from '../core/services/notification';
 
 import LoggedHeader from '../components/dashboard/logged-header';
 import Sidebar from '../components/dashboard/sidebar';
@@ -11,6 +12,18 @@ import '../styles/my-land.css';
 
 function MyLand() {
   const test = [1, 2, 3, 4, 5, 6, 7, 8];
+  const deleteLand = () => {
+    notificationService
+      .showError({
+        title: 'Delete Land',
+        message: 'Are you sure you want to delete?',
+        resolveLabel: 'Delete',
+        rejectLabel: 'Cancel'
+      })
+      // eslint-disable-next-line no-console
+      .then(() => console.log('Land deleted'));
+  };
+
   return (
     <div>
       <LoggedHeader siteTitle="Farm Voice" />
@@ -27,19 +40,27 @@ function MyLand() {
             </div>
             <Row>
               {test.map((item) => (
-                  <Col key={item} lg={4} sm={6} className="single-land">
-                    <Card bg="light" text="dark">
-                      <Card.Header>700 <sup>2</sup> M - <span className="medium-text">Rent</span></Card.Header>
-                      <Card.Body>
-                        <Card.Title>The Land title here</Card.Title>
-                        <Card.Text className="medium-text">
-                          Cameroon, South-West, Tiko
-                          <br />
-                          0 <strong>XAF</strong>
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
+                <Col key={item} lg={4} sm={6} className="single-land">
+                  <Card bg="light" text="dark">
+                    <Card.Header>
+                      700 <sup>2</sup> M - <span className="medium-text">Rent</span>
+                    </Card.Header>
+                    <Card.Body>
+                      <Card.Title>The Land title here</Card.Title>
+                      <Card.Text className="medium-text">
+                        Cameroon, South-West, Tiko
+                        <br />0 <strong>XAF</strong>
+                        <br />
+                        <div className="land-icon-section">
+                          <span className="edit-text">Edit</span>
+                          <span className="del-text medium-text" onClick={deleteLand}>
+                            Delete
+                          </span>
+                        </div>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
               ))}
             </Row>
           </div>
